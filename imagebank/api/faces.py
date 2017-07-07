0deef233497b1e446df57256ff66a58163ecdef8
Minh Nguyen <minh.nguyen98@gmail.com>
2017-07-07 10:38:57 +0700

    Commented embed.py, faces.py, pipeline.py, clean, dbclean

--- imagebank/api/faces.py
+++ imagebank/api/faces.py
@@ -1,67 +1,100 @@
-import sys, os, io, time
-import pickle, requests, threading
+import os
+import io
+import sys
+import time
+import pickle
+import requests
 import threading
 import numpy as np
-from functools import wraps
-from scipy import spatial
 from PIL import Image
+from scipy import spatial
+from functools import wraps
+
 import face_recognition
 
 
 class Faces(object):
 
     def __init__(self):
+        # Open .pickle files - our database
         self.database = 'database/'
         with open(os.path.join(self.database, 'names.pickle'), 'rb') as handle:
             self.names = pickle.load(handle)
         with open(os.path.join(self.database, 'data.pickle'), 'rb') as handle:
             self.embeddings = pickle.load(handle)
 
+        # cKDTree is used for finding closest embedding by L2 norm
         self.tree = spatial.cKDTree(self.embeddings, leafsize=100)
+        # The *currently* arbitrary threshold of embedding similarity
         self.threshold = 0.43
 
+        # API endpoint for retreiving images
         self.imagebank = 'http://127.0.0.1:8000/imagebank/'
 
     def identify(self, id):
+        """Identify input face using the database"""
+
+        # Fetch raw data from API endpoint
         data = requests.get(self.imagebank + str(id) + '/data/', stream=True)
+        # Convert to numpy array
         array = np.array(Image.open(io.BytesIO(data.raw.read())))
+        # Obtain face embedding
         embedding = face_recognition.face_encodings(array)
 
+        # If embedding is found
         if embedding:
+            # Find nearest embedding in database
             dist, ind = self.tree.query(embedding[0])
-            #rtn = self.names[ind] + ' ' + str(dist)
+            # If that embedding is close enough, return the embedding info
             if dist < self.threshold:
                 rtn = self.names[ind] + ' ' + str(dist)
+            # Else, the face is not in the database
             else:
                 rtn = 'Face not in database'
+        # No face found in image
         else:
             rtn = 'No face found'
 
         return rtn
 
     def update(self, id):
+        """Add/update face to database"""
+
+        # Fetch name and data from API endpoints
         name = requests.get(self.imagebank + str(id) + '/').json()['name']
         data = requests.get(self.imagebank + str(id) + '/data/', stream=True)
+        # Convert to numpy array
         array = np.array(Image.open(io.BytesIO(data.raw.read())))
+        # Obtain face embedding
         embedding = face_recognition.face_encodings(array)
 
+        # If embedding is found
         if embedding:
+            # Find name already in database
             if name in self.names:
+                # Find index
                 index = self.names.index(name)
+                # Update existing embedding
                 self.embeddings[index] = (
                     self.embeddings[index] + embedding[0]) / 2
                 rtn = 'Updated ' + name + ' embedding'
+            # Name not in database
             else:
+                # Add name and data to database
                 self.names.append(name)
                 self.embeddings.append(embedding[0])
+                # Recreate cKDTree
                 self.tree = spatial.cKDTree(self.embeddings)
                 rtn = 'Added ' + name + ' embedding'
+        # No face found in image
         else:
             rtn = 'No face found'
 
         return rtn
 
     def save(self):
+        """Periodically pickle current database and save"""
+
         threading.Timer(10, self.save).start()
         with open(os.path.join(self.database, 'names.pickle'), 'wb') as handle:
             pickle.dump(self.names, handle, protocol=2)
--- pipeline.py
+++ pipeline.py
@@ -1,25 +1,39 @@
-import sys, io
-from PIL import Image
-import numpy as np
-import face_recognition
+import io
+import sys
 import requests
+import numpy as np
+from PIL import Image
+
+import face_recognition
+
 
 def main():
+    """Face detection to API pipeline"""
+
+    # Get image path
     imagepath = sys.argv[1]
+    # Get image name
     im_name = imagepath.split('/')[-1]
 
+    # Obtain face locations
     im = Image.open(imagepath)
     locs = face_recognition.face_locations(np.array(im))
     print(len(locs), 'faces detected')
+    # If any faces found
     if locs:
+        # Go through each face
         for loc in locs:
-            face = im.crop((loc[3], loc[0], loc[1], loc[2]))    
+            # Crop face
+            face = im.crop((loc[3], loc[0], loc[1], loc[2]))
+            # Get bytes from face image
             facebytes = io.BytesIO()
             face.save(facebytes, format='JPEG')
-            # im.show()
-            data = {'name':''}
+            # Send a HTTP POST request and print response
+            data = {'name': ''}
             files = {'image': (im_name, facebytes.getvalue())}
-            resp = requests.post('http://localhost:8000/imagebank/', auth=('admin', 'adminadmin'), data=data, files=files)
+            auth = ('admin', 'adminadmin')
+            resp = requests.post(
+                'http://localhost:8000/imagebank/', auth=auth, data=data, files=files)
             print(resp.json())
 
 if __name__ == '__main__':
