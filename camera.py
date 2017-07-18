bee6093f0dda2d3983de6b938131a1013c8f369e
Minh Nguyen <minh.nguyen98@gmail.com>
2017-07-18 12:40:52 +0700

    Camera uses matplotlib for 'video' data

--- camera.py
+++ camera.py
@@ -1,16 +1,24 @@
 import sys
 import cv2
 import requests
-from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 import face_recognition
 
 
 def main():
+    plt.ion()
+    fig, ax = plt.subplots(figsize=(8,4.5))
+    fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
+    image = ax.imshow(np.zeros((720, 1280, 3)))
+    ax.axis('tight')
+    ax.axis('off')
+
     ipport = sys.argv[1]
     video_capture = cv2.VideoCapture(0)
 
     framecount = 0
-    while framecount < 30:
+    while True:
         framecount += 1
         _, frame = video_capture.read()
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -28,15 +36,12 @@ def main():
                 resp = requests.post(
                     'http://' + ipport + '/imagebank/', auth=auth, data=data, files=files)
                 print(resp.json())
-            
-                cv2.rectangle(frame, (4*left, 4*top), (4*right, 4*bottom), (0,0,255), 2)
-                if type(resp.json()) is dict:
-                    cv2.putText(frame, resp.json()['name'], (4*left+6, 4*bottom-6), font, 1.0, (255,255,0), 1)
-                else:
-                    cv2.putText(frame, resp.json()[0], (4*left+6, 4*bottom-6), font, 1.0, (255,0,0), 1)
+                if resp.json() != ['No faces found']:
+                    cv2.rectangle(frame, (4*left, 4*top), (4*right, 4*bottom), (0,0,255), 2)
+                    cv2.putText(frame, resp.json()[0]['name'], (4*left+6, 4*bottom-6), font, 1.0, (255,255,0), 1)
         print()
-        im = Image.fromarray(frame)
-        im.show()
+        image.set_data(frame)
+        fig.canvas.flush_events()
     
     video_capture.release()
 
